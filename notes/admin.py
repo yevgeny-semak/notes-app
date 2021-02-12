d2f0716dcf6b5877b0dce64133058b5415299c4b
YevgenySemak <Yevgeny.Semak@itechart-group.com>
2021-02-12 11:40:25 +0300

    added user model

--- notes/admin.py
+++ notes/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
-from notes.models import Note
+from notes.models import Note, User
 
 
 class NoteAdmin(admin.ModelAdmin):
     model = Note
-    list_display = ('title', 'created_at', 'updated_at',)
+    list_display = ('id', 'title', 'created_at', 'updated_at',)
     list_display_links = ('title',)
     search_fields = ('title',)
     ordering = ('title',)
 
 
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ('id', 'username', 'email')
+    list_display_links = ('username',)
+    search_fields = ('username',)
+    ordering = ('username', 'created_at',)
+
+
 admin.site.register(Note, NoteAdmin)
+admin.site.register(User, UserAdmin)
--- notes/models.py
+++ notes/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 
 class Note(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.username
