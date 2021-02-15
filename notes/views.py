2db9dd99077f18bcf2a424e4785b1404171ab972
euseand <48668000+euseand@users.noreply.github.com>
2021-02-15 16:17:30 +0300

    fix: function names + blank lines

--- notes/views.py
+++ notes/views.py
@@ -44,5 +44,5 @@ class NotesItemView(APIView):
         serializer = NoteSerializer(note, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
