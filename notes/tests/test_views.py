08e16f7b3c2da863a130177d259cf28ef9c8c899
euseand <48668000+euseand@users.noreply.github.com>
2021-02-15 13:58:29 +0300

    feat: added update functionality for notes + tests

--- notes/tests/test_views.py
+++ notes/tests/test_views.py
@@ -74,3 +74,36 @@ class GetSingleNoteTest(APITestCase):
             reverse('notes_item', kwargs={'pk': 10})
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class UpdateNoteTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test', password='1234', email='test@localhost')
+        self.note1 = Note.objects.create(title='test1', content='testcontent1', user=self.user)
+        self.note2 = Note.objects.create(title='test2', content='testcontent2', user=self.user)
+        self.valid_payload = {
+            'title': 'test42',
+            'content': 'testcontent42',
+            'user': self.user.id
+        }
+        self.invalid_payload = {
+            'title': '',
+            'content': 'testcontent43',
+            'user': self.user.id
+        }
+
+    def test_valid_update_note(self):
+        response = self.client.put(
+            reverse('notes_item', kwargs={'pk': self.note1.id}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_update_note(self):
+        response = self.client.put(
+            reverse('notes_item', kwargs={'pk': self.note1.id}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- notes/views.py
+++ notes/views.py
@@ -26,11 +26,23 @@ class NotesView(APIView):
 
 
 class NotesItemView(APIView):
-    def get(self, request, pk):
+    @staticmethod
+    def get_object(pk):
         try:
-            note = Note.objects.get(pk=pk)
+            return Note.objects.get(pk=pk)
         except Note.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
+        
+    def get(self, request, pk):
+        note = self.get_object(pk)
         serializer = NoteSerializer(note)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
