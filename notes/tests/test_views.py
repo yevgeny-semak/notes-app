ddc54d9b243ae3a3f32d0710212d1f0b150f5808
YevgenySemak <Yevgeny.Semak@itechart-group.com>
2021-02-19 11:35:46 +0300

    fix: updated tests with token auth

--- notes/tests/test_views.py
+++ notes/tests/test_views.py
@@ -1,6 +1,7 @@
 import json
 
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
@@ -11,12 +12,16 @@ from notes.serializers import NoteSerializer
 class GetAllNotesTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
+        self.token = Token.objects.create(user=self.user)
         Note.objects.create(title='test1', content='testcontent1', user=self.user)
         Note.objects.create(title='test2', content='testcontent2', user=self.user)
         Note.objects.create(title='test2', content='testcontent3', user=self.user)
 
     def test_get_all_notes(self):
-        response = self.client.get(reverse('notes'))
+        response = self.client.get(
+            reverse('notes'),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
+        )
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         self.assertEqual(response.data, serializer.data)
@@ -26,30 +31,33 @@ class GetAllNotesTest(APITestCase):
 class CreateNewNoteTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
+        self.token = Token.objects.create(user=self.user)
         self.valid_payload = {
             'title': 'test1',
-            'content': 'testcontent1',
-            'user': self.user.id
+            'content': 'testcontent1'
         }
         self.invalid_payload = {
             'title': '',
-            'content': 'testcontent1',
-            'user': self.user.id
+            'content': 'testcontent1'
         }
 
     def test_create_note_with_valid_payload(self):
+        self.client.force_login(user=self.user)
         response = self.client.post(
             reverse('notes'),
             data=json.dumps(self.valid_payload),
-            content_type='application/json'
+            content_type='application/json',
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_note_with_invalid_payload(self):
+        self.client.force_login(user=self.user)
         response = self.client.post(
             reverse('notes'),
             data=json.dumps(self.invalid_payload),
-            content_type='application/json'
+            content_type='application/json',
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -58,10 +66,13 @@ class GetSingleNoteTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
         self.note = Note.objects.create(title='test1', content='testcontent1', user=self.user)
+        self.token = Token.objects.create(user=self.user)
 
     def test_get_note_with_valid_pk(self):
+        self.client.force_login(user=self.user)
         response = self.client.get(
-            reverse('notes_item', kwargs={'pk': self.note.pk})
+            reverse('notes_item', kwargs={'pk': self.note.pk}),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         note = Note.objects.get(pk=self.note.pk)
         serializer = NoteSerializer(note)
@@ -69,8 +80,10 @@ class GetSingleNoteTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_note_with_invalid_pk(self):
+        self.client.force_login(user=self.user)
         response = self.client.get(
-            reverse('notes_item', kwargs={'pk': 10})
+            reverse('notes_item', kwargs={'pk': 10}),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -79,22 +92,22 @@ class UpdateNoteTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
         self.note = Note.objects.create(title='test1', content='testcontent1', user=self.user)
+        self.token = Token.objects.create(user=self.user)
         self.valid_payload = {
             'title': 'test42',
-            'content': 'testcontent42',
-            'user': self.user.id
+            'content': 'testcontent42'
         }
         self.invalid_payload = {
             'title': '',
-            'content': 'testcontent43',
-            'user': self.user.id
+            'content': 'testcontent43'
         }
 
     def test_update_note_with_valid_payload(self):
         response = self.client.put(
             reverse('notes_item', kwargs={'pk': self.note.pk}),
             data=json.dumps(self.valid_payload),
-            content_type='application/json'
+            content_type='application/json',
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -102,7 +115,8 @@ class UpdateNoteTest(APITestCase):
         response = self.client.put(
             reverse('notes_item', kwargs={'pk': self.note.pk}),
             data=json.dumps(self.invalid_payload),
-            content_type='application/json'
+            content_type='application/json',
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -111,16 +125,19 @@ class DeleteSingleNoteTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
         self.note = Note.objects.create(title='test1', content='testcontent1', user=self.user)
+        self.token = Token.objects.create(user=self.user)
 
     def test_delete_note_with_valid_pk(self):
         response = self.client.delete(
             reverse('notes_item', kwargs={'pk': self.note.pk}),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_note_with_invalid_pk(self):
         response = self.client.delete(
             reverse('notes_item', kwargs={'pk': 10}),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -129,11 +146,11 @@ class GetAutoGeneratedNoteTest(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(username='test', password='1234', email='test@localhost')
         self.note = Note.objects.create(title='Autogenerated Note 1', content='Note content', user=self.user)
-        self.client.user = self.user
+        self.token = Token.objects.create(user=self.user)
 
     def test_get_autogenerated_note(self):
-        self.client.force_authenticate(user=self.user)
         response = self.client.post(
             reverse('autogenerated_note'),
+            HTTP_AUTHORIZATION=f'Token {self.token}'
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
