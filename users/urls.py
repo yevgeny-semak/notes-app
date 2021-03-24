9f7acd5109a3ad6886771021549bc77efd9cfb15
euseand <Yevgeny.Semak@itechart-group.com>
2021-03-25 00:28:06 +0300

    feat: added logout view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from users.views import TokenObtainPairWithUserInfoView, CustomUserRegisterView
+from users.views import CustomUserRegisterView
+from users.views import LogoutAndBlacklistRefreshTokenForUserView, TokenObtainPairWithUserInfoView
 
 urlpatterns = [
     path('users/register/', CustomUserRegisterView.as_view(), name="user_register"),
     path('token/obtain/', TokenObtainPairWithUserInfoView.as_view(), name='token_obtain'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='token_blacklist')
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,28 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users import serializers
+
+
+class CustomUserRegisterView(APIView):
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    @staticmethod
+    def post(request):
+        serializer = serializers.CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.save()
+            data = serializer.data
+            return Response({'user': data}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TokenObtainPairWithUserInfoView(TokenObtainPairView):
@@ -12,17 +30,16 @@ class TokenObtainPairWithUserInfoView(TokenObtainPairView):
     serializer_class = serializers.CustomTokenObtainPairSerializer
 
 
-class CustomUserRegisterView(APIView):
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
     permission_classes = (AllowAny,)
+    authentication_classes = ()
 
     @staticmethod
     def post(request):
-        serializer = serializers.CustomUserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        user = serializer.save()
-        if user:
-            data = serializer.data
-            return Response(data=data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            token_refresh = request.data["token_refresh"]
+            token = RefreshToken(token_refresh)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
