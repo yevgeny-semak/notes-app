d476e4d3dcb40f5d0e653c8eb053d1d36f999c25
euseand <Yevgeny.Semak@itechart-group.com>
2021-03-24 23:21:32 +0300

    fix: fixed notes views tests

--- notes/tests/test_views.py
+++ notes/tests/test_views.py
@@ -37,7 +37,7 @@ class GetUserNotesTest(APITestCase):
         )
         notes = Note.objects.filter(user=self.user.id)
         serializer = NoteSerializer(notes, many=True)
-        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.data, {'notes': serializer.data})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
@@ -86,7 +86,7 @@ class GetSingleNoteTest(APITestCase):
         )
         note = Note.objects.get(pk=self.note.pk)
         serializer = NoteSerializer(note)
-        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.data, {'note': serializer.data})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_note_with_invalid_pk(self):
