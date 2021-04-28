92d64fc623f4810e40778dc57206b3ced5da79ea
euseand <Yevgeny.Semak@itechart-group.com>
2021-04-28 23:49:17 +0300

    feat: altered notes

--- notes/admin.py
+++ notes/admin.py
@@ -5,10 +5,10 @@ from notes.models import Note
 
 class NoteAdmin(admin.ModelAdmin):
     model = Note
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'user')
-    list_display_links = ('title',)
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'user',)
+    list_display_links = ('title', 'content',)
     list_filter = ('user',)
-    readonly_fields = ('created_at', 'updated_at',)
+    readonly_fields = ('created_at', 'updated_at', 'sort_order')
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
--- notes/models.py
+++ notes/models.py
@@ -6,15 +6,18 @@ from users.models import CustomUser
 class Note(models.Model):
     title = models.CharField(max_length=50, blank=True)
     content = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    sort_order = models.DateTimeField(auto_now_add=True)
+    is_archived = models.BooleanField(default=False)
+    is_pinned = models.BooleanField(default=False)
     user = models.ForeignKey(CustomUser, related_name='notes_user', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        db_table='notes_note'
+        db_table = 'notes_note'
         verbose_name = 'Note'
         verbose_name_plural = 'Notes'
         ordering = ['-created_at', 'title']
