87e524beb4deee963d9d11cc0290a0568573c55e
euseand <Yevgeny.Semak@itechart-group.com>
2021-04-30 12:47:00 +0300

    fix: updated note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from users.models import CustomUser
@@ -8,13 +10,19 @@ class Note(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    sort_order = models.DateTimeField(auto_now_add=True)
+    sort_order = models.IntegerField(null=True, blank=True)
     is_archived = models.BooleanField(default=False)
     is_pinned = models.BooleanField(default=False)
     user = models.ForeignKey(CustomUser, related_name='notes_user', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.sort_order:
+            self.sort_order = self.pk
+            super().save(update_fields=['sort_order'])
 
     class Meta:
         db_table = 'notes_note'
